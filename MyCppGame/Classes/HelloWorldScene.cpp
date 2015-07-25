15ad9dc72d6fc43e2c6e9b9e2cbe3b5dc66e88f1
Javier Fuchs <Javier.X.Fuchs.-ND@disney.com>
2015-07-25 05:24:15 -0300

    Adding menu: like and grid options.

--- MyCppGame/Classes/HelloWorldScene.cpp
+++ MyCppGame/Classes/HelloWorldScene.cpp
@@ -1,6 +1,6 @@
-#include "HelloWorldScene.h"
-
 USING_NS_CC;
+
+#include "HelloWorldScene.h"
 
 Scene* HelloWorld::createScene()
 {
@@ -27,54 +27,127 @@ bool HelloWorld::init()
         return false;
     }
     
+    HelloWorld::configureMenu();
+    
+    HelloWorld::createCloseButton();
+    
+    HelloWorld::configureTitle();
+    
+    HelloWorld::configureImage();
+    
+    HelloWorld::configureMenu();
+    
+    return true;
+}
+
+
+MenuItemImage* HelloWorld::createCloseButton()
+{
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
+    
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
-
+    
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
                                            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
+    
+    return closeItem;
+    
+}
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+MenuItemLabel* HelloWorld::createLikeButton()
+{
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    auto like = Label::createWithTTF("[ like ]", "fonts/arial.ttf", 30);
+
+    auto likeButton = MenuItemLabel::create(like, CC_CALLBACK_1(HelloWorld::menuLikeCallback, this));
+    
+    likeButton->setPosition(Vec2(origin.x + visibleSize.width - like->getContentSize().width,
+                                 origin.y + visibleSize.height - like->getContentSize().height));
+    
+    return likeButton;
+
+}
+
+MenuItemLabel* HelloWorld::createGridButton()
+{
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    auto grid = Label::createWithTTF("[ grid ]", "fonts/arial.ttf", 30);
+    
+    auto button = MenuItemLabel::create(grid, CC_CALLBACK_1(HelloWorld::menuGridCallback, this));
+    
+    button->setPosition(Vec2(origin.x + grid->getContentSize().width,
+                           origin.y + visibleSize.height - grid->getContentSize().height));
+    
+    return button;
+}
+
+void HelloWorld::configureTitle()
+{
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     /////////////////////////////
     // 3. add your codes below...
-
+    
     // add a label shows "Hello World"
     // create and initialize a label
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    auto label = Label::createWithTTF("Capybara of the day", "fonts/Marker Felt.ttf", 50);
     
     // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
                             origin.y + visibleSize.height - label->getContentSize().height));
-
+    
     // add the label as a child to this layer
     this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
-    return true;
 }
 
+void HelloWorld::configureImage()
+{
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("capybara.jpg");
+    
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+}
+
+void HelloWorld::configureMenu()
+{
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(HelloWorld::createCloseButton(),
+                             HelloWorld::createLikeButton(),
+                             HelloWorld::createGridButton(), NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+}
+
+void HelloWorld::menuLikeCallback(Ref* pSender)
+{
+
+}
+
+void HelloWorld::menuGridCallback(Ref* pSender)
+{
+}
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
--- MyCppGame/Classes/HelloWorldScene.h
+++ MyCppGame/Classes/HelloWorldScene.h
@@ -3,8 +3,24 @@
 
 #include "cocos2d.h"
 
+using namespace cocos2d;
+
 class HelloWorld : public cocos2d::Layer
 {
+
+private:
+    MenuItemImage* createCloseButton();
+    MenuItemLabel* createLikeButton();
+    MenuItemLabel* createGridButton();
+    void configureTitle();
+    void configureImage();
+    void configureMenu();
+    
+    // a selector callback
+    void menuLikeCallback(cocos2d::Ref* pSender);
+    void menuGridCallback(cocos2d::Ref* pSender);
+    void menuCloseCallback(cocos2d::Ref* pSender);
+
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
@@ -12,11 +28,9 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
     
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+    
 };
 
 #endif // __HELLOWORLD_SCENE_H__
